f832a8473427b1d5610ad348ae259909e7cadd83
Vanessa Dean <vanessa.dean@gmail.com>
2014-01-03 08:24:07 -0500

    Add content and social bar

---
+++ mailer.php
@@ -0,0 +1,47 @@
+<?php
+	###################################
+	# Jonathan Grover - Simple Mailer #
+	# v0.1 Beta for PHP 5             #
+	# Date Modified: 12/22/2012       #
+	# MIT License (see details below) #
+    ###################################
+
+	// Set email address to send to i.e: you@yoursite.com
+	$sendTo		=	'vanessa.dean@gmail.com';
+
+	// Set email subject (appears as subject)
+	$subject 	= 	'Traveling Van - Contact Form Submission';
+
+	/* No need to touch the code below this line! */
+
+	// get posted form values
+	$name 		= 	$_POST['name'];
+	$email 		= 	$_POST['email'];
+	$message 	= 	$_POST['message'];
+
+	// setup email header
+	$header 	= 	"From: ".$email;
+
+	// construct email body 
+	$body 		=	"Name: ".$name."\r\n".
+					"Email: ".$email."\r\n".
+					"\r\n".
+					"Message: ".$message;
+
+		// send email
+		mail($sendTo, $subject, $body, $header);
+		
+		//go to success page
+		header('Location: contact.html');
+		exit;
+
+	/*
+	Copyright (c) 2013 Jonathan Grover
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	*/
+?>
